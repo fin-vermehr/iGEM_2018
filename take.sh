aa1ab3ce84bc66b8043a79a7b31371d4f6c442e3
Cait Harrigan <cait.harrigan@mail.utoronto.ca>
2018-06-07 03:08:21 +0000

    quiet test

--- take.sh
+++ take.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
+#number of stills to take
 n=3
+#time between stills
 t=5
+#no batch name by default
 
 
-
-while getopts "n:t:" opt; do
+while getopts "n:t:b:" opt; do
     case "$opt" in
     n) n=$OPTARG
         ;;
     t) t=$OPTARG
         ;;
+    b) b=$OPTARG
+	;;
     esac
 done
 
 
 for i in `seq 1 $n`; 
 do
-	DATE=$(date +"%Y-%m-%d_%H%M")"_"$i
-	raspistill -hf -o /home/igem/Desktop/capture/$DATE.jpg
-
-#	git add /home/igem/Desktop/capture/$DATE.jpg
+	FNAME=$(date +"%Y-%m-%d_%H%M")"_"$b$i
+	raspistill -hf -o /home/igem/iGEM_2018/capture/$FNAME.jpg
+	echo "picture taken"$FNAME".jpg"
+#	git add /home/igem/iGEM_2018/capture/$FNAME.jpg
 #	git commit -m "automated upload"
 #	git push
+	echo "picture uploaded"
 
-    sleep $t
+    	sleep $t
 
 done
 
