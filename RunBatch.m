2179cbde14fae542fa68d9f45755ccab68cb3bb5
Brayden <brayden.kell@gmail.com>
2018-10-01 20:14:05 -0400

    updated bioreactor

--- RunBatch.m
+++ RunBatch.m
@@ -1,4 +1,4 @@
-function RunBatch(image_folder, VALIDATE)
+function ClusterInf = RunBatch(image_folder, VALIDATE)
 %image_folder is folder containing your image(s), dont put file name at the
 %end (ex: 'C:/Users/user/my_images')
 %VALIDATE is a boolean flag for displaying plots that look pretty and or
@@ -10,27 +10,22 @@ if ~isdir(image_folder)
     return;
 end
 
-batch_dir = dir(image_folder);
-batch_dir = batch_dir(~ismember({batch_dir.name},{'.','..'}));
-num_images = size(batch_dir,1);
+batch_dir = dir(image_folder); %directory struct with images
+batch_dir = batch_dir(~ismember({batch_dir.name},{'.','..'})); %remove these non-file entries
+num_images = size(batch_dir,1); %number of images contained in folder (assuming nothing else stored here)
 
-bands = cell(1,num_images); %will later store row and column of pixel we identify in each picture as our pixel of interest
-heights = zeros(1, num_images); %will later store conversion of pixel location to height in lab frame
-MAX = zeros(1, num_images);
-positions = cell(1, num_images);
+%bands = cell(1,num_images); %will later store row and column of pixel we identify in each picture as our pixel of interest
+%heights = zeros(1, num_images); %will later store conversion of pixel location to height in lab frame
+%MAX = zeros(1, num_images);
+
+positions = cell(1, num_images); %for each image there will be a set of objects resulting from the segmentation and labelling for which positions will be stored
+LabeledImages = cell(1,num_images); %store labelled in images in cell array
+NumObjects = zeros(1,num_images); %for each image we will store the number of objects identified by segmentation and labelling
 for i = 1:num_images
-    fname = batch_dir(i).name;
-    fname = strcat(image_folder,'\',fname);
-    I = imread(fname);
-    %{
-    Ir = I(:,:,1);
-    Ig = I(:,:,2);
-    Ib = I(:,:,3);
-    
-    Q = Ir-Ig-Ib;
-    Q=Q>0;
-    Q = double(Q);
-    %}
+    fname = batch_dir(i).name; %name of current frame
+    fname = strcat(image_folder,'\',fname); %constructing fullpath
+    I = imread(fname); %get pixel data for current frame
+
     if i==1
         figure;
         title('Crop out graduated cylinder ensuring bottom is bottom of cylinder');
@@ -40,31 +35,38 @@ for i = 1:num_images
     else
         J = imcrop(I,rect); %on subsequent iterations will apply cropping region defined on first iteration
     end
-    
-    Jr = J(:,:,1);  %this is just separating colour channels, will be useful later
+    %below is a few lines of code for separating color channels for
+    %potential analysis later to determine fraction of floating cells that
+    %are expressing gas vesicles (the will be expressing blue chromophore
+    %AND RFP)
+    Jr = J(:,:,1);  
     Jg = J(:,:,2);
     Jb = J(:,:,3);
     
-    G = rgb2gray(J);
-    level = graythresh(G);
-    BW = imbinarize(G,level);
-    %s = regionprops(BW, G, 'WeightedCentroid');
+    G = rgb2gray(J); %convert to gray scale
+    level = graythresh(G); %determine threshold level using Otsu's method
+    BW = imbinarize(G,level); %convert to binary image
+    %BW = imcomplement(BW);
     
-    L = bwlabeln(BW, 18); %labelling objects
-    Lrgb = label2rgb(L,'jet','w','shuffle');
+    [L,n] = bwlabeln(BW, 18); %labelling objects
+    NumObjects(i) = n; %count objects
+    Lrgb = label2rgb(L,'jet','w','shuffle'); %converts labelling matrix to an RGB map so that different objects can more easily be identified graphically
     
-    Lfilt = medfilt2(L, [5 5]); %filter image to remove noise
+    Lfilt = medfilt2(L, [5 5]); %apply simple median filter on image to remove noise
     %TODO fine tune region size (i.e. [5 5]) when we get real images
-    Lfilt = label2rgb(Lfilt,'jet','w','shuffle');
     
-    s = regionprops(Lfilt, 'Centroid');
+    Lfiltrgb = label2rgb(Lfilt,'jet','w','shuffle');
+    LabeledImages{i} = Lfiltrgb; %store ith (current) rgb label matrix
     
-    num_obj = numel(s);
-    centroids = cell(1, num_obj);
+    s = regionprops(BW, 'Centroid'); %perform centroiding get ordered pairs (y,z), z height along cylinder principle axis
+    %y increases to right and is zero at left cylinder egdge
+    
+    num_obj = numel(s); %count object (this should be the same as n above and is probably redundant)
+    centroids = zeros(num_obj,2); %store ordered pairs in array centroids(k,:) returns [y z] for the kth object
     for k = 1 : num_obj
-        centroids{k} = [s(k).Centroid(1), s(k).Centroid(2)];
+        centroids(k,:) = [s(k).Centroid(1), s(k).Centroid(2)];
     end
-    positions{i} = centroids;
+    positions{i} = centroids; %store these matrices of arbitrary size for each frame in cell array
     
     %these are just some useful plots for verifying code is performing as
     %expected
@@ -76,7 +78,7 @@ for i = 1:num_images
         hold off;
         
         figure;
-        imshow(Lfilt);
+        imshow(Lfiltrgb);
         hold on;
         numObj = numel(s);
         for k = 1 : numObj
@@ -86,7 +88,7 @@ for i = 1:num_images
         hold off;
         
         figure;
-        imshow(Lfilt);
+        imshow(Lfiltrgb);
         hold on;
         numObj = numel(s);
         for k = 1 : numObj
@@ -96,7 +98,7 @@ for i = 1:num_images
         hold off;
         
         figure;
-        imshow(Lfilt);
+        imshow(Lfiltrgb);
         hold on;
         numObj = numel(s);
         for k = 1 : numObj
@@ -112,7 +114,7 @@ for i = 1:num_images
         hold off
         
         figure;
-        imshow(Lfilt);
+        imshow(Lfiltrgb);
         hold on;
         title('Filtered Labeled Image');
         hold off;
@@ -140,46 +142,112 @@ for i = 1:num_images
     MAXs(i) = M; %might be useful to see how the max mean row value changes as a function of time to infer some sort of time dependent distribution
 end
     %}
-    
-    
 end
+
 %Temporal Tracking
 
-ClusterInf = cell(num_images),1);
-info = struct;
-ClusterInf{1} = info;
+ClusterInf = cell(num_images,1); %for each image, there will exist and info struct and we will store them all here
+info = struct; %instantiate struct object to input info for first frame
 
 for i=2:num_images
-   current_posns = positions{i};
-   previous_posns = positions{i-1};
-   num_current_obj = length(current_posns);
-   num_prev_obj = length(previous_posns);
-   
-   info = struct;
-   ClusterInf{i} = info;
-   
-   inds = zeros(num_current_obj,1);
-   norms = zeros(num_current_obj,1);
-   cents = cell(num_current_obj,1);
-   cents_prev = cell(num_current_obj,1);
-   for k = 1:num_current_obj
-       dists = zeros(num_prev_obj,1);
-       for l = 1:num_prev_obj
-           d = EuclideanNorn2D(current_posns{k}, previous_posns{l});
-           dists(l) = d;    
-       end
-       [d, ind] = min(dists);
-       inds(k) = ind;
-       norms(k) = d;
-       cents{k} = current_posns{k};
-       cents_prev{k} = previous_posns{ind};
-       
-       info(k).centroid_current = current_posns{k};
-       info(k).centroid_previous = previous_posns{ind};
-       info(k).distance = d;
-       info(k).previous_frame_index = ind;
-       info(k).previous_frame_label = 
-       info(k).new_label = 
-   end
+    current_posns = positions{i}; %load positions of all objects in current frame as num current objects x 2 array
+    previous_posns = positions{i-1}; %load positions of all objects from previous frame as num previous objects x 2 array
+    num_current_obj = size(current_posns,1); %num objects in current frame
+    num_prev_obj = size(previous_posns,1); %num objects from previous frame
+    
+    info = struct; %instantiate struct to input info for the (i+1)th frame
+    
+    %inds = zeros(num_current_obj,1);
+    %norms = zeros(num_current_obj,1);
+    %cents = cell(num_current_obj,1);
+    %cents_prev = cell(num_current_obj,1);
+    
+    %temporal clustering relies on mapping all labels in current frame to
+    %the closest object in the previous frame and assinging labels
+    %accordingly (note: multiple objects in frame (i+1) can be mapped to
+    %the same object in the ith frame. This corresponds to and object
+    %splitting into multiple)
+    for k = 1:num_current_obj
+        dists = zeros(num_prev_obj,1); %store euclidean distance between the kth object in the current frame and each object in the previous frame
+        for l = 1:num_prev_obj
+            d = EuclideanNorm2D(current_posns{k}, previous_posns{l}); %distance between kth object in (i+1)th frame and lth object in ith frame
+            dists(l) = d;
+        end
+        [d, ind] = min(dists); %we care about the object in the previous frame that is closest to the kth object in the current fram so we minimize and get index
+        %inds(k) = ind;
+        %norms(k) = d;
+        %cents{k} = current_posns{k};
+        %cents_prev{k} = previous_posns{ind};
+        
+        y = round(previous_posns{ind}(1)); %round to convert from floating point spatial values to integer pixel coordinates
+        x = round(previous_posns{ind}(2));
+        
+        info(k).centroid_current = current_posns(k,:); %store the ordered pair for the kth object in the (i+1)th frame
+        info(k).centroid_previous = previous_posns(ind,:);  %store the ordered pair for the closest object in the ith frame to the kth object in the current frame
+        info(k).distance = d; %store the distance between the kth object in current frame and the object we map it to in previous frame
+        info(k).previous_frame_index = ind; %this might be not be needed, but might as well store too much info than too little
+        oldLabel = LabeledImages{i-1}(x,y); %this is the label of the object in the previous frame that is closest to the kth object in the current frame
+        info(k).previous_frame_label = old_label; %store label from previous frame
+        
+        if num_current_obj == num_prev_obj
+            newLabel = oldLabel; %in the case that there exists the same number of objects, there is no need to update label
+        else
+            newLabel = 'temp'; %store a placeholder until re-labelling algorithm... mostly unecessessary, but is useful for debugging
+        end
+        
+        info(k).new_label = newLabel;
+    end
+    %re-labelling algorithm:
+    if num_current_obj > num_prev_obj 
+        
+        delta = num_current_obj - num_prev_obj;
+        inds = zeros(delta,2);
+        cnt = 0;
+        
+        prev_X = zeros(1,num_current_obj);
+        prev_Y = zeros(1,num_current_obj);
+        for k=1:num_current_obj
+            prev_X(k) = info(k).centroid_previous(1);
+            prev_Y(k) = info(k).centroid_previous(2);
+            
+            X = info(k).centroid_previous(1);
+            Y = info(k).centroid_previous(2);
+            %indx = find(ismember(prev_X, X));
+            %indy = find(ismember(prev_Y, Y));
+            indx = ismember(prev_X, X);
+            indy = ismember(prev_Y, Y);
+            not_zero = indx(ismember(indx,1));
+            if isequal(indx,indy) && numel(not_zero)>1 && cnt<=delta
+                cnt = cnt+1;
+                ind = find(indx); %this is an array of indexes of the objects that share a common label in previous frame
+                inds(k,:) = ind;
+            end
+            if cnt==delta
+               break 
+            end
+        end
+        %inds = inds(~ismember(inds,0)); %remove zeros
+        %inds = unique(inds,'rows'); %remove dupicate rows... found better
+        %way to ensure no zeros or duplicates
+        
+        %{
+        [nx, binx] = histc(prev_X, unique(prev_Y));
+        multiple_x = find(nx>1);
+        indx = find(ismember(binx, multiple_x));
+        [ny, biny] = histc(prev_Y, unique(prev_Y));
+        multiple_y = find(ny>1);
+        indy = find(ismember(biny, multiple_y));
+        %}
+        for k=1:size(inds,1)
+           for l=1:size(inds,2)
+               if l == 1
+                   info(inds(k,l)).new_Label = info(inds(k,l)).previous_Label;
+               else
+                  info(inds(k,l)).new_Label = num_prev_obj + (k-1);
+               end
+           end
+        end
+    end
+    ClusterInf{i} = info;
 end
 end
--- bioreactor_model.m
+++ bioreactor_model.m
@@ -1,32 +1,40 @@
-gamma = 0.5;
-kappa = 0.01;
-Xwm = 5000;
-D = 0.2;
-beta = 0.03;
-alpha = 5;
-S_0 = 200;
-mu_m = 0.35;
-K_s = 80;
-Xuo = 100;
-delta = 1.5;
+gamma = 0.5; %yield constant (?)
+kappa = 0.01; %death rate h^-1
+%Xwm = 5000; %maximal area mass density of surface attachment mg/cm^2 not used?
+D = 0.2; %dilution/flow rate h^-1
+beta = 0.03; %detachment rate of particle from biomass surface h^-1
+alpha = 5; %attachment rate h^-1
+S_0 = 200; %concentration of substrate in incoming feed mol L^-1
+mu_m = 0.35; %maximum specific growth rate (monod equation) unitless
+K_s = 80; %monod constont mol L^-1
+Xuo = 50; %initial concentration of unbound particle mol L^-1
+delta = 1.5; %
 
 f = @(t,x) [x(1) * (((mu_m * x(2)) / (K_s + x(2))) - D - kappa);
             D * (S_0 - x(2)) - 1 / gamma * ((mu_m * x(2)) / (K_s + x(2)) * x(1));
-            D * (Xuo - x(3)) + beta * x(4) - alpha * x(3) * (1 - x(4) / (delta * x(1)));
-            -D * x(4) - beta * x(4) + alpha * x(3) * (1 - x(4) / (delta * x(1)));]
+            D * (Xuo-x(5) - x(3)) + beta * x(4) - alpha * x(3) * (1 - x(4) / (delta * x(1)));
+            -D * x(4) - beta * x(4) + alpha * x(3) * (1 - x(4) / (delta * x(1)));
+            D*x(4)];
         
-        
-[t,xa] = ode45(f,[0 250],[20 40 100 0])
+[t,xa] = ode45(f,[0 250],[20 40 Xuo 0 0]);
+fprintf('ode solved\n')
 
-plot(t,xa(:,1));
+ind = find((Xuo-xa(:,5))<=0);
+tp = t(1:ind(1));
+xap = xa(1:ind(1),:);
+
+figure;
+plot(tp,xap(:,1)); %Cell concetration mol L^-1
 hold on 
-plot(t,xa(:,2));
+plot(tp,xap(:,2)); %Substrate concentration mol L^-1
 hold on
-plot(t,xa(:,3))
+plot(tp,xap(:,3)) %?
 hold on
-plot(t,xa(:,4))
+plot(tp,xap(:,4)) %?
+hold on
+plot(tp, Xuo - xap(:,5)) %Concentration remaining in in system of particle mol L^-1
 set(findall(gca, 'Type', 'Line'),'LineWidth',2);
 
-legend('E. coli Pop', 'Substrate','Floating', 'Adhered')
+legend('E. coli Pop', 'Substrate','Floating', 'Adhered', 'Particle left in system')
 xlabel('Time (t)'), ylabel('y(t)    ')
 
